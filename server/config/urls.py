7d5ab86b1b3d38043b417c7b4e1a6305a1a86671
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2025-05-10 15:06:40 -0400

    Rferash token

--- server/config/urls.py
+++ server/config/urls.py
@@ -8,6 +8,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework_simplejwt import views as jwt_views
 from django.conf.urls.i18n import i18n_patterns
+from rest_framework_simplejwt import views as jwt_views
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -27,6 +28,7 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('authen/', include('authen.urls')),
     path('api/', include('brick_main.urls')),
 ]
